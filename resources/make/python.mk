fa05a44378e2c6fea2bcdd5abe434f9f831329a8
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-18 20:16:51 -0600

    Added a target for the Python interpreter.
    This includes all Python project library paths.

--- resources/make/python.mk
+++ resources/make/python.mk
@@ -1,5 +1,7 @@
-VENV=python/.venv
-REQS=python/requirements.txt
+ERL_PORT_LIB=deps/erlport/priv/python3
+LIB=python
+VENV=$(LIB)/.venv
+REQS=$(LIB)/requirements.txt
 
 venv:
 	python3 -m venv $(VENV)
@@ -9,3 +11,8 @@ get-py-deps:
 	pip3 install -r $(REQS)
 
 python: venv get-py-deps
+
+interp:
+	@. $(VENV)/bin/activate && \
+	PYTHONPATH=$(LIB):$(ERL_PORT_LIB) \
+	python3
