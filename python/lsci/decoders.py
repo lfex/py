2b7f141db80257829f6a51ac6213b13d8f6ae317
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-20 23:35:58 -0600

    Fixed up de/en-coders.

--- python/lsci/decoders.py
+++ python/lsci/decoders.py
@@ -1,11 +1,14 @@
 from scipy.interpolate import interpolate
 
+from lsci import logger
+
 
 def interp1d(value):
     if isinstance(value, tuple) and len(value) == 2:
         if value[0] == b"interp1d":
-            (x, y, kind, axis, copy, bounds_error,
-             fill_value, assume_sorted) = value[1]
+            logger.debug(value)
+            (x, y, kind, axis, copy, bounds_error) = value[1]
+            kind = kind.decode("utf-8")
             value = interpolate.interp1d(
                 x, y, kind=kind, axis=axis, copy=copy,
                 bounds_error=bounds_error)
--- python/lsci/encoders.py
+++ python/lsci/encoders.py
@@ -2,10 +2,14 @@ from erlport.erlterms import Atom
 
 from scipy.interpolate import interpolate
 
+from lsci import logger
+
 
 def interp1d(value):
     if isinstance(value, interpolate.interp1d):
-        data = (value.x, value.y, value._kind, value.axis, value.copy,
+        logger.debug(value)
+        kind = bytes(value._kind, "utf-8")
+        data = (value.x, value.y, kind, value.axis, value.copy,
                 value.bounds_error)
         value = (b"interp1d", data)
     return value
