c6f7e328cc9ad2661bb963e30743f3c52448fb3e
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-20 20:56:22 -0600

    Cleaned up functions.

--- python/lsci/obj.py
+++ python/lsci/obj.py
@@ -9,21 +9,30 @@ def kwargs_keys_to_strings(kwargs):
     return [(x.decode("utf-8"), y) for (x, y) in kwargs]
 
 
-def call_method(obj, funcname, args=[], kwarg_list=None):
-    if len(kwarg_list) == 0:
-        kwarg_list = []
+def decode_args(arg_list):
+    new_args = []
+    for arg in arg_list:
+        if isinstance(arg, bytes):
+            arg = arg.decode("utf-8")
+        new_args.append(arg)
+    return new_args
+
+
+def decode_kwargs(kwarg_list):
     new_kwarg_list = []
     for (key, val) in kwarg_list:
         if isinstance(val, bytes):
             val = val.decode("utf-8")
         new_kwarg_list.append([key, val])
-    kwargs = dict(kwargs_keys_to_strings(new_kwarg_list))
-    new_args = []
-    for arg in args:
-        if isinstance(arg, bytes):
-            arg = arg.decode("utf-8")
-        new_args.append(arg)
-    return attr(obj, funcname)(*new_args, **kwargs)
+    return dict(kwargs_keys_to_strings(new_kwarg_list))
+
+
+def call_method(obj, funcname, args=[], kwarg_list=None):
+    if len(kwarg_list) == 0:
+        kwarg_list = []
+    kwargs = decode_kwargs(kwarg_list)
+    args = decode_args(args)
+    return attr(obj, funcname)(*args, **kwargs)
 
 
 def call_func(modname, funcname, args=[], kwarg_list=None):
@@ -31,5 +40,5 @@ def call_func(modname, funcname, args=[], kwarg_list=None):
     return call_method(module, funcname, args, kwarg_list)
 
 
-def call_callable(func, args=[], kwarg_list=None):
-    return call_method(func, "__call__", args, kwarg_list)
+def call_callable(func_obj, args=[], kwarg_list=None):
+    return call_method(func_obj, b"__call__", args, kwarg_list)
