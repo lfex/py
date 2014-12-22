fdb9ce0964198e632c42490e27914ce647c48a87
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-21 23:04:54 -0600

    Raised the logging threshold.

--- python/lsci/logger.py
+++ python/lsci/logger.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(
     format='[%(asctime)s] [%(levelname)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M',
-    level=logging.INFO)
+    level=logging.WARN)
 
 
 warn = logging.warning
