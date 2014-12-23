5fbfba645684332d24d7ad65e6a26b0f45ef6939
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-23 16:59:35 -0600

    Added date, etc. en/decoders.

--- python/lfe/decoders.py
+++ python/lfe/decoders.py
@@ -1,6 +1,8 @@
+from datetime import date, datetime, time, timedelta
+
 from cytoolz.functoolz import compose
 
-from lfe import logger
+from lfe import logger, erlang
 
 
 def dicts(value):
@@ -11,5 +13,42 @@ def dicts(value):
     return value
 
 
+def dates(value):
+    if (isinstance(value, tuple)
+        and len(value) == 2
+        and value[0] == erlang.List("date")):
+        value = date(*list(value[1]))
+    return value
+
+
+def datetimes(value):
+    if (isinstance(value, tuple)
+        and len(value) == 2
+        and value[0] == erlang.List("datetime")):
+        (year, month, day, hour, minute, second, micro, tz) = value[1]
+        value = datetime(year, month, day, hour, minute, second, micro, tz)
+    return value
+
+
+def times(value):
+    if (isinstance(value, tuple)
+        and len(value) == 2
+        and value[0] == erlang.List("time")):
+        value = time(*list(value[1]))
+    return value
+
+
+def timedeltas(value):
+    if (isinstance(value, tuple)
+        and len(value) == 2
+        and value[0] == erlang.List("timedelta")):
+        value = timedelta(*list(value[1]))
+    return value
+
+
 def get_all(value):
-    return compose(dicts)(value)
+    return compose(dicts,
+                   dates,
+                   datetimes,
+                   times,
+                   timedeltas)(value)
--- python/lfe/encoders.py
+++ python/lfe/encoders.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime, time, timedelta
+
 from cytoolz.functoolz import compose
 
 from lfe import logger
@@ -9,5 +11,36 @@ def dicts(value):
     return value
 
 
+def dates(value):
+    if isinstance(value, date):
+        value = ("date", (value.year, value.month, value.day))
+    return value
+
+
+def datetimes(value):
+    if isinstance(value, datetime):
+        value = ("datetime", (value.year, value.month, value.day,
+                               value.hour, value.minute, value.second,
+                               value.microsecond, value.tzinfo))
+    return value
+
+
+def times(value):
+    if isinstance(value, time):
+        value = ("time", (value.hour, value.minute, value.second,
+                           value.microsecond, value.tzinfo))
+    return value
+
+
+def timedeltas(value):
+    if isinstance(value, timedelta):
+        value = ("timedelta", value.days, value.seconds, value.microseconds)
+    return value
+
+
 def get_all(value):
-    return compose(dicts)(value)
+    return compose(dicts,
+                   dates,
+                   datetimes,
+                   times,
+                   timedeltas)(value)
