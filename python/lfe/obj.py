bd4e90443e883e7f996227901938ad9516a85c54
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-24 00:54:36 -0600

    Added support for arg and kwarg data type encoding.
    Also:
     * Improved module-getting code.
     * Added const function.

--- python/lfe/obj.py
+++ python/lfe/obj.py
@@ -1,18 +1,39 @@
 import importlib
 
-from lfe import logger
+from lfe import decoders, erlang, logger
 
 
 def init():
     "Set up module in ErlPort Python namespace."
+
 
 def echo(*args, **kwargs):
     "Useful for debugging module-level issues."
     return (args, kwargs)
 
 
-def attr(obj, attr):
-    return getattr(obj, attr.decode("utf-8").replace("-", "_"))
+def get_module(modname):
+    modname = modname.decode("utf-8")
+    try:
+        module = importlib.import_module(modname)
+    except ImportError as err:
+        parts = modname.split(".")
+        modname = parts[0]
+        submod = parts[1]
+        try:
+            parent_module = importlib.import_module(modname)
+            module = attr(parent_module, bytes(submod, "utf-8"))
+        except:
+            raise(err)
+    return module
+
+
+def attr(obj, attr_name):
+    return getattr(obj, attr_name.decode("utf-8").replace("-", "_"))
+
+
+def const(modname, attr_name):
+    return attr(get_module(modname), attr_name)
 
 
 def kwargs_keys_to_strings(kwargs):
@@ -24,6 +45,7 @@ def decode_args(arg_list):
     for arg in arg_list:
         if isinstance(arg, bytes):
             arg = arg.decode("utf-8")
+        arg = decoders.decode(arg)
         new_args.append(arg)
     return new_args
 
@@ -33,6 +55,7 @@ def decode_kwargs(kwarg_list):
     for (key, val) in kwarg_list:
         if isinstance(val, bytes):
             val = val.decode("utf-8")
+        val = decoders.decode(val)
         new_kwarg_list.append([key, val])
     return dict(kwargs_keys_to_strings(new_kwarg_list))
 
@@ -46,7 +69,7 @@ def call_method(obj, funcname, args=[], kwarg_list=None):
 
 
 def call_func(modname, funcname, args=[], kwarg_list=None):
-    module = importlib.import_module(modname.decode("utf-8"))
+    module = get_module(modname)
     return call_method(module, funcname, args, kwarg_list)
 
 
