d9f9d1090e7a1f4fff89f6e9732b56400966ef86
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-12-28 01:01:28 -0600

    Updated description.

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(
   name: "py",
-  description: "Python from Erlang/LFE: A Wrapper for ErlPort with Convenience Functions",
+  description: "Distributed Python for the Erlang Ecosystem",
   version: "0.0.1",
   keywords: ["LFE", "Lisp", "Library", "API", "ErlPort", "Python"],
   maintainers: [[name: "Duncan McGreggor", email: "oubiwann@lfe.io"]],
