6a44469d5d36b99e87b029518308e33460b1b5c3
Duncan McGreggor <oubiwann@gmail.com>
2016-03-06 13:19:50 -0600

    Updated to work on Ubuntu (manually pull down get-pip.py).

--- resources/make/python.mk
+++ resources/make/python.mk
@@ -2,13 +2,19 @@ ERL_PORT_LIB=deps/erlport/priv/python3
 LIB=python
 VENV=$(LIB)/.venv
 REQS=$(LIB)/requirements.txt
+GET_PIP=$(LIB)/get-pip.py
 
 venv:
-	python3 -m venv $(VENV)
+	python3 -m venv --without-pip $(VENV)
 
-get-py-deps:
+$(GET_PIP):
+	wget -O $(GET_PIP) https://bootstrap.pypa.io/get-pip.py
 	. $(VENV)/bin/activate && \
-	pip3 install -r $(REQS)
+	python3 $(GET_PIP)
+
+get-py-deps: $(GET_PIP)
+	. $(VENV)/bin/activate && \
+	pip install -r $(REQS)
 
 python: venv get-py-deps
 
