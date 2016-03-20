bab85a1f8a4a22c0ab53a28fe0051141a5c66e32
Duncan McGreggor <oubiwann@gmail.com>
2016-03-20 16:25:24 -0500

    Updated targets for LFE v1.0.

--- resources/make/common.mk
+++ resources/make/common.mk
@@ -11,60 +11,31 @@ HOST = $(shell scutil --get ComputerName)
 endif
 
 LIB = $(PROJECT)
-DEPS = ./deps
 BIN_DIR = ./bin
 SOURCE_DIR = ./src
 OUT_DIR = ./ebin
-TEST_DIR = ./test
-TEST_OUT_DIR = ./.eunit
-SCRIPT_PATH=$(DEPS)/lfe/bin:.:./bin:"$(PATH)":/usr/local/bin
-ifeq ($(shell which lfetool),)
-LFETOOL=$(BIN_DIR)/lfetool
-else
-LFETOOL=lfetool
-endif
-ERL_LIBS=..:../$(PROJECT):$(shell $(LFETOOL) info erllibs)
+LFE=_build/default/lib/lfe/bin/lfe
 
 $(BIN_DIR):
 	mkdir -p $(BIN_DIR)
 
-get-lfetool: $(BIN_DIR)
-	curl -L -o ./lfetool https://raw.github.com/lfe/lfetool/stable/lfetool && \
-	chmod 755 ./lfetool && \
-	mv ./lfetool $(BIN_DIR)
-
 get-version:
-	@PATH=$(SCRIPT_PATH) $(LFETOOL) info version
-	@echo "Erlang/OTP, LFE, & library versions:"
-	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) erl \
-	-eval "lfe_io:format(\"~p~n\",['$(PROJECT)-util':'get-versions'()])." \
-	-noshell -s erlang halt
+	@echo "Get LFE py version:"
+	@$(LFE) \
+	-eval '(lfe_io:format "~p~n" (list (py-util:get-py-version)))' -noshell -s erlang halt
 
-get-deps:
-	@echo "Getting dependencies ..."
-	@which rebar.cmd >/dev/null 2>&1 && rebar.cmd get-deps || rebar get-deps
+get-versions:
+	@echo "Erlang/OTP, LFE, & library versions:"
+	@$(LFE) \
+	-eval '(progn (py:start) (lfe_io:format "~p~n" (list (py-util:get-versions))))' -noshell -s erlang halt
 
 clean-ebin:
 	@echo "Cleaning ebin dir ..."
 	@rm -f $(OUT_DIR)/*.beam
 
-clean-eunit:
-	-@PATH=$(SCRIPT_PATH) $(LFETOOL) tests clean
-
-proj-compile: get-deps clean-ebin
+proj-compile: clean-ebin
 	@echo "Compiling project code and dependencies ..."
-	@mkdir -p ebin
-	@cp src/$(PROJECT).app.src $(OUT_DIR)/$(PROJECT).app
-	@which rebar.cmd >/dev/null 2>&1 && \
-	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
-	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile
-
-proj-compile-no-deps: clean-ebin
-	@echo "Compiling only project code ..."
-	@which rebar.cmd >/dev/null 2>&1 && \
-	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) \
-	rebar.cmd compile skip_deps=true || \
-	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile skip_deps=true
+	@rebar3 compile
 
 compile-tests: clean-eunit
 	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests build
@@ -72,44 +43,13 @@ compile-tests: clean-eunit
 repl: proj-compile
 	@which clear >/dev/null 2>&1 && clear || printf "\033c"
 	@echo "Starting an LFE REPL ..."
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) repl lfe -s $(PROJECT)
+	@$(LFE) -s $(PROJECT)
 
-repl-no-deps: proj-compile-no-deps
-	@which clear >/dev/null 2>&1 && clear || printf "\033c"
-	@echo "Starting an LFE REPL ..."
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) repl lfe -s $(PROJECT)
+clean: clean-ebin
+	@echo "Cleaning project build dir ..."
+	@rm -rf ebin/* _build/default/lib/py/ebin/*
 
-shell: proj-compile
-	@which clear >/dev/null 2>&1 && clear || printf "\033c"
-	@echo "Starting an Erlang shell ..."
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) erl -s $(PROJECT)
+check:
+	@rebar3 as tests eunit
 
-shell-no-deps: proj-compile-no-deps
-	@which clear >/dev/null 2>&1 && clear || printf "\033c"
-	@echo "Starting an Erlang shell ..."
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) erl -s $(PROJECT)
-
-clean: clean-ebin clean-eunit
-	@which rebar.cmd >/dev/null 2>&1 && rebar.cmd clean || rebar clean
-
-check-unit-only:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests unit
-
-check-integration-only:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests integration
-
-check-system-only:
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests system
-
-check-unit-with-deps: get-deps proj-compile proj-compile-tests check-unit-only
-check-unit: clean-eunit proj-compile-no-deps check-unit-only
-check-integration: clean-eunit proj-compile check-integration-only
-check-system: clean-eunit proj-compile check-system-only
-check-all-with-deps: clean-eunit proj-compile check-unit-only \
-	check-integration-only check-system-only clean-eunit
-check-all: get-deps clean-eunit proj-compile-no-deps
-	@PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) $(LFETOOL) tests all
-
-check: check-unit-with-deps
-
-check-travis: $(LFETOOL) check
+check-travis: check
